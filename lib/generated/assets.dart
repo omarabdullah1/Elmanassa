4ade3f7671a7af545ace995d0d19deff8a3b7b32
Omar Abdullah <36677597+omarabdullah1@users.noreply.github.com>
2023-12-26 19:24:18 +0400

    assets update

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -15,20 +15,6 @@ class Assets {
   static const String imagesOnboard3 = 'assets/images/onboard3.png';
   static const String imagesPerson = 'assets/images/person.png';
   static const String imagesVideoPlay = 'assets/images/videoPlay.png';
-  static const String mOCUPMOC10Apple iPhone 11 Pro Max Screenshot 2 = 'assets/MOCUP/MOC10Apple iPhone 11 Pro Max Screenshot 2.png';
-  static const String mOCUPMOC11Apple iPhone 11 Pro Max Screenshot 3 = 'assets/MOCUP/MOC11Apple iPhone 11 Pro Max Screenshot 3.png';
-  static const String mOCUPMOC12Apple iPhone 11 Pro Max Screenshot 4 = 'assets/MOCUP/MOC12Apple iPhone 11 Pro Max Screenshot 4.png';
-  static const String mOCUPMOC13Apple iPhone 11 Pro Max Screenshot 5 = 'assets/MOCUP/MOC13Apple iPhone 11 Pro Max Screenshot 5.png';
-  static const String mOCUPMOC14Apple iPhone 11 Pro Max Screenshot 6 = 'assets/MOCUP/MOC14Apple iPhone 11 Pro Max Screenshot 6.png';
-  static const String mOCUPMOC1Apple iPhone 11 Pro Max Screenshot 0 = 'assets/MOCUP/MOC1Apple iPhone 11 Pro Max Screenshot 0.png';
-  static const String mOCUPMOC2Apple iPhone 11 Pro Max Screenshot 1 = 'assets/MOCUP/MOC2Apple iPhone 11 Pro Max Screenshot 1.png';
-  static const String mOCUPMOC3Apple iPhone 11 Pro Max Screenshot 2 = 'assets/MOCUP/MOC3Apple iPhone 11 Pro Max Screenshot 2.png';
-  static const String mOCUPMOC4Apple iPhone 11 Pro Max Screenshot 3 = 'assets/MOCUP/MOC4Apple iPhone 11 Pro Max Screenshot 3.png';
-  static const String mOCUPMOC5Apple iPhone 11 Pro Max Screenshot 4 = 'assets/MOCUP/MOC5Apple iPhone 11 Pro Max Screenshot 4.png';
-  static const String mOCUPMOC6Apple iPhone 11 Pro Max Screenshot 5 = 'assets/MOCUP/MOC6Apple iPhone 11 Pro Max Screenshot 5.png';
-  static const String mOCUPMOC7Apple iPhone 11 Pro Max Screenshot 6 = 'assets/MOCUP/MOC7Apple iPhone 11 Pro Max Screenshot 6.png';
-  static const String mOCUPMOC8Apple iPhone 11 Pro Max Screenshot 0 = 'assets/MOCUP/MOC8Apple iPhone 11 Pro Max Screenshot 0.png';
-  static const String mOCUPMOC9Apple iPhone 11 Pro Max Screenshot 1 = 'assets/MOCUP/MOC9Apple iPhone 11 Pro Max Screenshot 1.png';
   static const String mOCUPPresentation1 = 'assets/MOCUP/Presentation1.png';
   static const String mOCUPPresentation2 = 'assets/MOCUP/Presentation2.png';
 
