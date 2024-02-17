7f70016ee4a3d775e7cea2d08ddc526cd74e7cac
Omar Abdullah <36677597+omarabdullah1@users.noreply.github.com>
2024-02-17 04:42:09 +0200

    ..f

--- lib/constants/end_points.dart
+++ lib/constants/end_points.dart
@@ -14,13 +14,21 @@ const supportPath = 'support-info';
 
 
 //////////////////////// STUDENT ///////////////////////////////////////////
-const userLoginPath = 'auth/student/login';
-const studentRegisterPath = 'auth/student/register';
-const subscribeToCourseByIdStudentPath = 'auth/student/subscriptions/subscribe-by-id';
-const subscribeToCourseByCodeStudentPath = 'auth/student/subscriptions/subscribe-by-code';
-const coursePackagesStudentPath = 'auth/student/subscriptions/course-packages';
-const enrolledCoursesStudentPath = 'auth/student/courses/enrolled';
-const courseDashboardStudentPath = 'auth/student/courses/dashboard';
+const userLoginPath = 'login';
+const studentRegisterPath = 'student/register';
+const subscribeToCourseByIdStudentPath = 'student/subscriptions/subscribe-by-id';
+const subscribeToCourseByCodeStudentPath = 'student/subscriptions/subscribe-by-code';
+const coursePackagesStudentPath = 'student/subscriptions/course-packages';
+const enrolledCoursesStudentPath = 'student/courses/enrolled';
+const courseDashboardStudentPath = 'student/courses/course-dashboard';
+const notificationStudentPath = 'student/notifications';
+const quizDetailsStudentPath = 'student/quizzes/details';
+const startQuizByCodeStudentPath = 'student/quizzes/start-by-code';
+const submitQuizquestionStudentPath = 'student/quizzes/question-submit';
+const submitQuizStudentPath = 'student/quizzes/submit';
+const getQuizsStudentPath = 'student/quizzes';
+const profileStudentPath = 'profile';
+const logoutStudentPath = 'logout';
 ////////////////////////////////////////////////////////////////////////////
 
 
