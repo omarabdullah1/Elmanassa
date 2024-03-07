4334f364bdbe29aaa1f453f956da12491160fc24
Omar Abdullah <36677597+omarabdullah1@users.noreply.github.com>
2024-03-08 00:45:24 +0200

    Merge remote-tracking branch 'origin/master' into drosak_online
    # Conflicts:
    #	android/app/build.gradle
    #	lib/business_logic/global_cubit/global_cubit.dart
    #	lib/business_logic/student_home_cubit/student_home_state.dart
    #	lib/presentation/screens/user/home/home_screen.dart
    #	lib/presentation/screens/user/profile/profile_screen.dart

--- lib/business_logic/global_cubit/global_cubit.dart
+++ lib/business_logic/global_cubit/global_cubit.dart
@@ -166,7 +166,7 @@ class GlobalCubit extends Cubit<GlobalState> {
         'password': password,
         'phone': phone,
         'gender': genderListEN[genderList.indexOf(selectedGender!)],
-        'level_id': allLevelsId![allLevelsTitle.indexOf(selectedLevel!)]
+        'level_id': allLevelsId![allLevelsTitle.indexOf(selectedLevel!)-1]
       },
     ).then((value) {
       studentRegisterModel = StudentRegisterModel.fromJson(value.data);
